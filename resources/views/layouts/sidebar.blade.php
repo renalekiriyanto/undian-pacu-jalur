ea5f0787fc23e08ac82c87b8d0c3edb4952c7533
rekto167 <ekysr99@gmail.com>
2024-06-27 07:27:45 +0700

    sidebar dipisah

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,36 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+                <i class="nav-icon fas fa-location-arrow"></i>
+                <p>
+                    Daerah
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+                <i class="nav-icon fas fa-building"></i>
+                <p>
+                    Arena
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+                <i class="nav-icon fas fa-ship"></i>
+                <p>
+                    Jalur
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pages/widgets.html" class="nav-link">
+                <i class="nav-icon fas fa-file"></i>
+                <p>
+                    Undian
+                </p>
+            </a>
+        </li>
+    </ul>
+</nav>
