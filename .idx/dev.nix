fd6ad23aebfe171067cc8abb49633c3636f72ae6
ekysr99 <ekysr99@gmail.com>
2024-07-02 00:56:42 +0000

    nix configuration

---
+++ .idx/dev.nix
@@ -0,0 +1,33 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.php83
+    pkgs.php83Packages.composer
+  ];
+  services.mysql = {
+  enable = true;
+  package = pkgs.mariadb;
+};
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
